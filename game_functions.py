9a81bd4774726a477717169386134f3995d6d0db
Angelos Pappas <ap1315@exeter.ac.uk>
2026-01-16 23:28:46 +0000

    pick_value() completed

--- game_functions.py
+++ game_functions.py
@@ -2,8 +2,11 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
-    return x
+    # spent so long trying to implement binary search 
+    # but then i realised the array is updated naturally 
+    # so all i had to do was guess the middle
+    mid = len(poss_values)//2
+    return poss_values[mid]
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
